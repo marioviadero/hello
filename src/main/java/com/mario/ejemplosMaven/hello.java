44811f545a85e350b09939b9f2633b60b1f6e0c8
Mario Viadero <mvh87@outlook.es>
2018-11-09 10:02:20 +0100

    Pruebas

--- src/main/java/com/mario/ejemplosMaven/hello.java
+++ src/main/java/com/mario/ejemplosMaven/hello.java
@@ -1,7 +1,11 @@
 package com.mario.ejemplosMaven;
 
 import org.apache.commons.codec.digest.DigestUtils;
-
+/**
+ * Clase con metodos que reciben un valor y lo encriptan en sha256. Metodo main para probarlo
+ * @author Mario
+ * 
+ */
 public class hello {
 	public static void main(String[] args) {
 
